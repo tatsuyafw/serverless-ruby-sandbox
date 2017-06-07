943f13e4f0022b386cc877461021e6f8d005fb53
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2017-06-08 01:36:09 +0900

    Use Ruby on Lambda!

--- handler.js
+++ handler.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const exec = require('child_process').exec;
+
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-    }),
-  };
-  callback(null, response);
+  const child = exec('./lambdaRuby.rb ' + "'" + JSON.stringify(event) + "'", (err, stdout, stderr) => {
+    const body = JSON.parse(stdout);
+		const response = {
+			statusCode: 200,
+			body: JSON.stringify(body)
+		};
+		callback(null, response);
+  });
 };
