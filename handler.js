72ea0bb7b959141c7340992cb1aa93137694f189
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2017-06-08 01:37:28 +0900

    Untabify

--- handler.js
+++ handler.js
@@ -5,10 +5,10 @@ const exec = require('child_process').exec;
 module.exports.hello = (event, context, callback) => {
   const child = exec('./lambdaRuby.rb ' + "'" + JSON.stringify(event) + "'", (err, stdout, stderr) => {
     const body = JSON.parse(stdout);
-		const response = {
-			statusCode: 200,
-			body: JSON.stringify(body)
-		};
-		callback(null, response);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(body)
+    };
+    callback(null, response);
   });
 };
