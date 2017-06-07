995b89f615d9fcd031251ffc8db377cf0123c5c8
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2017-06-08 00:29:46 +0900

    Use an http event for lambda

--- handler.js
+++ handler.js
@@ -5,12 +5,7 @@ module.exports.hello = (event, context, callback) => {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
     }),
   };
-
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
